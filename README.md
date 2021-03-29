8cb83f79edcf156370cbb988d5a23392f781b818
Ben De Meester <ben.demeester@ugent.be>
2021-03-29 17:17:32 +0200

    updated script

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Repository for https://w3id.org/function/spec
 ## Quickstart
 
 - edit dev.html
-  - Makes sure all your local assets are in the `resources` folder, and the links in your dev.html file are relative (important because the publishing script creates multiple nested paths)
-- save as snapshot to function.html [using the respec functionality](https://respec.org/docs/#using-browser)
-- run `node publish.js` to get the index.html + archived version in the `dist` folder
+- Makes sure all your local assets are in the `resources` folder, and the links in your dev.html file are relative (important because the publishing script creates multiple nested paths)
+- save as snapshot to index.html [using the respec functionality](https://respec.org/docs/#using-browser)
+- run `node publish.js index.html` to get the index.html + archived version in the `dist` folder
+- copy the folder `./resources` into your `dist` folder.
 
 The `dist` folder contents should mimic the contents on `https://w3id.org/function/spec`
--- publish.js
+++ publish.js
@@ -1,18 +1,38 @@
 /**
  * Created by bjdmeest on 10/06/2016.
+ * Header comment and inline comments added by slieber on 18/01/2021
+ *
+ * Usually for specifications the most current version is available as index.html under the root address
+ * and additionally for each version a dedicated version folder exists. Each version refers with
+ * a link to its previous version (another folder under the root address with the version in its name)
+ *
+ * This script has the aim to populate a new version based on the provided content, therefore it:
+ * - sets correct current and previous dates in the provided content
+ * - creates a new dedicated version folder with the new content
+ * - replaces the current root index.html with the new content
  */
 
 var fs = require('fs');
 var path = require('path');
 
-var dateString = (new Date()).toISOString().split('T')[0].replace(/-/g, '');
+if (process.argv.length != 3) {
+    console.log("");
+    console.log("Please provide the name of the HTML file which should be used to populate the new version of the spec");
+    console.log("The placeholders %thisDate% and %prevDate% will be replaced with respective values.");
+    console.log("Usage: node publish.sh [spec HTML version]");
+    process.exit(1);
+}
+var inputFilename = process.argv[2];
 
+// Create a new directory for the new version within dist, e.g. dist/20210118
+var dateString = (new Date()).toISOString().split('T')[0].replace(/-/g, '');
 try {
     fs.mkdirSync(path.resolve(__dirname, 'dist', dateString));
 } catch (e) {
 
 }
 
+// Create a list of all folders in dist, i.e. resources folder and all previous version folders without the main index.html file
 var files = fs.readdirSync(path.resolve(__dirname, 'dist'));
 var dirs = [];
 for (var i = 0; i < files.length; i++) {
@@ -21,6 +41,7 @@ for (var i = 0; i < files.length; i++) {
     }
 }
 
+// Within the created list of directories, remove 'resources' and the version we just created
 dirs.sort();
 dirs.reverse();
 
@@ -32,10 +53,15 @@ if (dirs[0] === dateString) {
     dirs.splice(0, 1);
 }
 
-var html = fs.readFileSync('./function.html', 'utf8');
+// TODO copy resources folder each time.
+
+// Use the content of inputFilename as new content: replace date placeholders and put it as 'current version' in dist root
+var html = fs.readFileSync(inputFilename, 'utf8');
 html = html.replace(/%thisDate%/g, dateString);
 html = html.replace(/%prevDate%/g, dirs[0]);
 fs.writeFileSync(path.resolve(__dirname, 'dist', 'index.html'), html);
+
+// Next to the dist root, the current version will also reside in its dedicated version directory for which paths need to be adapted
 html = html.replace(/\.\/resources/g, '../resources');
 fs.writeFileSync(path.resolve(__dirname, 'dist', dateString, 'index.html'), html);
 
